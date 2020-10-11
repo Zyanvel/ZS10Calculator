a959a72bbe5ca643c7caabde85285b16370875b6
Zyanvel <zyanvel@gmail.com>
2020-10-11 00:28:30 -0700

    this is our first commit

---
+++ script.js
@@ -0,0 +1,3 @@
+let userAge = prompt("Hi there! May I ask how old are you? :D");
+let convertAge = userAge*365;
+alert(`Wow!, acording to my estimations, ${userAge} years is roughly ${convertAge} days. So cool! :)`);
